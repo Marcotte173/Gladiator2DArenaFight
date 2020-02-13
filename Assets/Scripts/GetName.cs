4d788c4ce514c45b83f91aea2049e52a8c7e3d99
Marcotte173 <47604041+Marcotte173@users.noreply.github.com>
2020-02-13 00:40:21 -0700

    Add files via upload

---
+++ Assets/Scripts/GetName.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GetName : MonoBehaviour
+{
+    public Text name;
+    public Stats stats;
+
+    private void Start()
+    {
+        name.text = stats.name;
+    }
+}
